1f801ab991aeb51e4be1ea70b4ba06acc51b1a3f
jiangkai <jiangkai.nju@gmail.com>
2013-04-06 10:55:48 +0800

    initialize

---
+++ yqhbase/src/main/java/com/yuqiao/yqhbase/App.java
@@ -0,0 +1,13 @@
+package com.yuqiao.yqhbase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
