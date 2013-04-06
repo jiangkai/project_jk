7b205214f270e5c34a4f7ea26b9f8a7e24db2d1b
jiangkai <jiangkai.nju@gmail.com>
2013-04-06 15:20:23 +0800

    get the pom.xml done

---
+++ yqhbase/src/main/java/com/yuqiao/yqhbase/HBaseServer.java
@@ -0,0 +1,5 @@
+package com.yuqiao.yqhbase;
+
+public interface HBaseServer {
+	//public 
+}
---
+++ yqhbase/src/main/java/com/yuqiao/yqhbase/impl/YQHBaseServerImpl.java
@@ -0,0 +1,7 @@
+package com.yuqiao.yqhbase.impl;
+
+import com.yuqiao.yqhbase.HBaseServer;
+
+public class YQHBaseServerImpl implements HBaseServer{
+
+}
