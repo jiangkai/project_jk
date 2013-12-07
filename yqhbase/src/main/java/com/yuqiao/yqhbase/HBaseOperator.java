50d1887156a0f432a6de8359dfeb03331d0435d5
jiangkai <jiangkai.nju@gmail.com>
2013-12-07 23:52:35 +0800

    initial

--- yqhbase/src/main/java/com/yuqiao/yqhbase/HBaseOperator.java
+++
@@ -1,9 +0,0 @@
-package com.yuqiao.yqhbase;
-
-import java.io.InputStream;
-
-public interface HBaseOperator {
-	public void write(String key,InputStream is);
-	public void write(String key,byte[] value);
-	public byte[] read(String key);
-}
--- yqhbase/src/main/java/com/yuqiao/yqhbase/HBaseServer.java
+++
@@ -1,6 +0,0 @@
-package com.yuqiao.yqhbase;
-
-public interface HBaseServer {
-	//public 
-	public HBaseOperator getHBaseOperator();
-}
